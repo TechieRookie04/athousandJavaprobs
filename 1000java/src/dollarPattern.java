daa93b4caf4e62edc371a2ca249271bce66b4a9f
TechieRookie04 <srivibha.vadrevu@gmail.com>
2022-06-07 03:33:24 +0530

    dollarPattern

---
+++ 1000java/src/dollarPattern.java
@@ -0,0 +1,28 @@
+
+public class dollarPattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i,j;
+		//String a[][] = new String[4][4];
+		String a= "$ ";
+		for(i=1;i<=4;i++) {
+			 for(j=1;j<=4;j++) { 
+				 if(i==2 || i==3) {
+					 if(j==3 || j==2) {
+						 System.out.print(" ");
+					 }
+					 else {
+						 System.out.print(a); 
+					 }
+				 }	
+				 else {
+				 System.out.print(a);
+				 }
+			} 
+			 System.out.println();
+		} 
+			 			
+		}
+
+}
